d995eec58f84e979ff6c7295fe9b93c5e9045009
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-11-26 12:59:46 -0500

    add test: TodoController

--- todoapp/src/test/java/com/shieldgenerator7/todoapp/TodoApplicationTests.java
+++ todoapp/src/test/java/com/shieldgenerator7/todoapp/TodoApplicationTests.java
@@ -5,7 +5,10 @@ import com.shieldgenerator7.todoapp.data.TodoList;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class TodoApplicationTests {
@@ -16,7 +19,6 @@ class TodoApplicationTests {
 
 	@Test
 	void testAddItem(){
-		final String taskHeader = "buy groceries";
 		TodoList list = new TodoList();
 		assertEquals(0, list.getCount());
 		Item item = new Item(taskHeader);
@@ -26,5 +28,20 @@ class TodoApplicationTests {
 		assertEquals(1, list.getCount());
 		assertEquals(taskHeader, list.getItem(0).getHeader());
 	}
+    final String taskHeader = "buy groceries";
+    @Test
+    void testController() {
+        TodoController controller = new TodoController();
+        List<String> todos = controller.getTodos();
+        assertNotNull(todos);
+        assertEquals(0, todos.size());
+
+        String addedTodo = controller.addTodo(taskHeader);
+        assertEquals(taskHeader, addedTodo);
+        todos = controller.getTodos();
+        assertNotNull(todos);
+        assertEquals(1, todos.size());
+        assertEquals(taskHeader, todos.get(0));
+    }
 
 }

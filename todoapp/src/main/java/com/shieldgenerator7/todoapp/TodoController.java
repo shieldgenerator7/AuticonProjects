1652965abdcc4dff47f2da14716d8cadd3aee35c
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-11-26 13:05:28 -0500

    Impement TodoController

--- todoapp/src/main/java/com/shieldgenerator7/todoapp/TodoController.java
+++ todoapp/src/main/java/com/shieldgenerator7/todoapp/TodoController.java
@@ -1,5 +1,6 @@
 package com.shieldgenerator7.todoapp;
 
+import com.shieldgenerator7.todoapp.data.Item;
 import com.shieldgenerator7.todoapp.data.TodoList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,11 +15,14 @@ public class TodoController {
 
     @GetMapping("/todos")
     public List<String> getTodos() {
-        return null;
+        return todoList.getTodos().stream()
+                .map(Item::getHeader).toList();
     }
 
     @PostMapping("/todos")
     public String addTodo(@RequestBody String todo) {
+        Item item = new Item(todo);
+        todoList.add(item);
         return todo;
     }
 

697864fb171109dd09587a54ec94a31d17b07b2b
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-11-26 14:28:09 -0500

    Update TodoApplicationTests.java

--- todoapp/src/test/java/com/shieldgenerator7/todoapp/TodoApplicationTests.java
+++ todoapp/src/test/java/com/shieldgenerator7/todoapp/TodoApplicationTests.java
@@ -2,18 +2,28 @@ package com.shieldgenerator7.todoapp;
 
 import com.shieldgenerator7.todoapp.data.Item;
 import com.shieldgenerator7.todoapp.data.TodoList;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodoApplicationTests {
 
     final String taskHeader = "buy groceries";
+
+    @Value(value="${local.server.port]")
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     void contextLoads() {

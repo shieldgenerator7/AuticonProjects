4e9c6a5c3b5e7c7cb7015d1841d93021e25971bd
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-11-26 12:11:13 -0500

    Add shell data classes

---
+++ todoapp/src/main/java/com/shieldgenerator7/todoapp/data/Item.java
@@ -0,0 +1,13 @@
+package com.shieldgenerator7.todoapp.data;
+
+public class Item {
+
+    public Item(String header){
+
+    }
+
+    public String getHeader(){
+        return "";
+    }
+
+}
---
+++ todoapp/src/main/java/com/shieldgenerator7/todoapp/data/TodoList.java
@@ -0,0 +1,26 @@
+package com.shieldgenerator7.todoapp.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoList {
+
+    private List<Item> todos = new ArrayList<>();
+
+    public void add(Item item){
+
+    }
+
+    public List<Item> getTodos(){
+        return null;
+    }
+
+    public int getCount(){
+        return 0;
+    }
+
+    public Item getItem(int index){
+        return null;
+    }
+
+}

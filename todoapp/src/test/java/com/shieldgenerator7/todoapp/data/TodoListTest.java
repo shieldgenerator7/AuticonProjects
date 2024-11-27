20c7c77382f8af725da9a287b3ec51bd36d4a597
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-11-27 11:21:51 -0500

    Add TodoListTest

---
+++ todoapp/src/test/java/com/shieldgenerator7/todoapp/data/TodoListTest.java
@@ -0,0 +1,32 @@
+package com.shieldgenerator7.todoapp.data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoListTest {
+
+    TodoList todoList;
+
+    @BeforeEach
+    void setUp() {
+        todoList = new TodoList();
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void getTodos() {
+    }
+
+    @Test
+    void getCount() {
+    }
+
+    @Test
+    void getItem() {
+    }
+}

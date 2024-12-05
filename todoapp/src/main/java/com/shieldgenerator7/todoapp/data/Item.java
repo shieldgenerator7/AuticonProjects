3265d90d35b771cdc005e1561247cc9f1f1a514f
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-12-04 21:26:58 -0500

    Import lombok

--- todoapp/src/main/java/com/shieldgenerator7/todoapp/data/Item.java
+++ todoapp/src/main/java/com/shieldgenerator7/todoapp/data/Item.java
@@ -3,6 +3,7 @@ package com.shieldgenerator7.todoapp.data;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
 public class Item {
@@ -11,8 +12,10 @@ public class Item {
     @GeneratedValue
     private Long id;
 
+    @Getter
     private String header;
 
+    @Getter
     private int completionStatus = 0;
 
     public Item (){
@@ -27,13 +30,6 @@ public class Item {
         return id;
     }
 
-    public String getHeader() {
-        return header;
-    }
-
-    public int getCompletionStatus(){
-        return completionStatus;
-    }
     public int setCompletionStatus(int value){
         completionStatus = Math.min(Math.max(value, 0), 100);
         return completionStatus;

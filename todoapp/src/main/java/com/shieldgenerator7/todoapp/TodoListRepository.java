ffb9ef41edd14543fd3d979a9cc92eb10476a140
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-12-16 11:20:50 -0500

    Add TodoListRepository.persist()

--- todoapp/src/main/java/com/shieldgenerator7/todoapp/TodoListRepository.java
+++ todoapp/src/main/java/com/shieldgenerator7/todoapp/TodoListRepository.java
@@ -1,9 +1,21 @@
 package com.shieldgenerator7.todoapp;
 
 import com.shieldgenerator7.todoapp.data.TodoList;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoListRepository extends JpaRepository<TodoList, Long> {
+
+    @Transactional
+    public default void persist() {
+        EntityManager entityManager = Persistence
+                .createEntityManagerFactory("com.shieldgenerator7.todoapp")
+                .createEntityManager();
+        entityManager.persist(this);
+    }
+
 }

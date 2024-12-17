afb8415cb6b35b9b31ee8be313a48901da57b643
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-12-17 14:23:46 -0500

    Use cors in server.js

--- todoapp-ui/server.js
+++ todoapp-ui/server.js
@@ -3,6 +3,12 @@
 
 var express = require('express');
 var app = express();
+
+//2024-12-17: copied from https://expressjs.com/en/resources/middleware/cors.html#configuration-options
+const cors = require('cors');
+app.use(cors());
+//
+
 app.use(express.static(__dirname + '/')); //__dir and not _dir
 var port = 8000; // you can use any port
 app.listen(port);

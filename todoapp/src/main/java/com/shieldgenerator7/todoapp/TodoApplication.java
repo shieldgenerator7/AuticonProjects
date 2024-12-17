718b310c42a2494f5061dbf3118a86235d2b05cb
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-12-17 11:13:09 -0500

    test code

--- todoapp/src/main/java/com/shieldgenerator7/todoapp/TodoApplication.java
+++ todoapp/src/main/java/com/shieldgenerator7/todoapp/TodoApplication.java
@@ -6,15 +6,43 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
+//@EnableTransactionManagement
+//@EnableJpaRepositories("com.shieldgenerator7.todoapp")
 public class TodoApplication {
+
+//    public static EntityManagerFactory emf;
+//    public static EntityManager em;
 
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
 
+//        emf = Persistence.createEntityManagerFactory("com.shieldgenerator7.todoapp");
+//        em = emf.createEntityManager();
+
+//        em.getTransaction().begin();
+//
+//        Item item1 = new Item("buy eggs");
+//        em.persist(item1);
+//        Item item2 = new Item("buy groceries");
+//        em.persist(item2);
+//
+//
+//        em.getTransaction().commit();
+//
+//        int key = 1;
+//        Item item = (Item)em.getReference(Item.class, key);
+//        if (item != null){
+//            System.out.println("item "+key+": "+ item.getHeader());
+//        }
+//        else{
+//            System.out.println("item "+key+": "+item);
+//        }
+//
+//        emf.close();
 
     }
 
--- todoapp/src/main/java/com/shieldgenerator7/todoapp/TodoListRepository.java
+++ todoapp/src/main/java/com/shieldgenerator7/todoapp/TodoListRepository.java
@@ -5,17 +5,43 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.Persistence;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoListRepository extends JpaRepository<TodoList, Long> {
 
-    @Transactional
-    public default void persist() {
-        EntityManager entityManager = Persistence
-                .createEntityManagerFactory("com.shieldgenerator7.todoapp")
-                .createEntityManager();
-        entityManager.persist(this);
-    }
+    //2024-12-10: copied from https://stackoverflow.com/a/35929057/2336212
+//    @Autowired
+//    private EntityManager entityManager;
+
+//    @Transactional
+//    public default void persist() {
+////        _checkEntityManager();
+//        EntityManager entityManager = TodoApplication.em;
+//        entityManager.getTransaction().begin();
+////                TodoApplication.emf
+////                .createEntityManager();
+//        entityManager.persist(this);
+//        entityManager.getTransaction().commit();
+////        entityManager.close();
+////        this.entityManager.persist(this);
+//    }
+
+//    @Transactional
+//    public TodoList merge(TodoList todolist) {
+//        _checkEntityManager();
+//        TodoList merged = this.entityManager.merge(todolist);
+//        this.entityManager.flush();
+//        return merged;
+//    }
+//
+//    private void _checkEntityManager(){
+//        if (this.entityManager == null) {
+//            this.entityManager = Persistence
+//                    .createEntityManagerFactory("com.shieldgenerator7.todoapp")
+//                    .createEntityManager();
+//        }
+//    }
 
 }

0ae14847ecb8e7fa6c6f0371822a1b0fec2b15d7
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-12-30 20:55:00 -0500

    Javadoc Item class

--- todoapp/src/main/java/com/shieldgenerator7/todoapp/data/Item.java
+++ todoapp/src/main/java/com/shieldgenerator7/todoapp/data/Item.java
@@ -19,27 +19,41 @@ public class Item {
     @ColumnDefault("0")
     private int completionStatus = 0;
 
-
-
     @Getter
     @Setter
     @Enumerated(EnumType.STRING)
     private Priority priority;
 
+    /**
+     * Constructs an Item with empty header and low priority
+     */
     public Item() {
         header = "";
         priority = Priority.LOW;
     }
 
+    /**
+     * Constructs an Item with the given header and low priority
+     * @param header The header for the Item
+     */
     public Item(String header) {
         this.header = header;
         priority = Priority.LOW;
     }
 
+    /**
+     * Returns the Id of this Item
+     * @return the Id of this Item
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the completion status of this Item to the given value, clamped to between 0 and 100, inclusive.
+     * @param value The value to set completion status to
+     * @return The resulting completion status, between 0 and 100, inclusive.
+     */
     public int setCompletionStatus(int value) {
         completionStatus = Math.min(Math.max(value, 0), 100);
         return completionStatus;
